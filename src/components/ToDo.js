e9941b49cbc2c92a8e8b9d97c5b0c0032400f354
Taylor Short <treddshort@gmail.com>
2018-05-06 14:50:11 -0700

    add key/values for todo list and created state/props for components.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,10 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     render() {
         return(
-           <li>A todo will go here</li> 
+           <li>
+               <input type="checkbox" checked={ this.props.isCompleted} />
+               <span>{ this.props.description }</span>
+           </li> 
         );
     }
 }
